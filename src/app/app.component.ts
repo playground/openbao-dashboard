4808f1d8c44c7be3dcbc6433685b7ab82562c144
Jeff Lu <ljeff@us.ibm.com>
2024-04-18 10:55:26 -0400

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'openbao-dashboard';
+  showMenu = false;
+
+  toggleNavbar() {
+    this.showMenu = !this.showMenu;
+  }
 }
