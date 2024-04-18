9ab29ad5385caa028882e8be5a3eb116ca0d2c6c
Jeff Lu <ljeff@us.ibm.com>
2024-04-18 12:07:46 -0400

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'openbao-dashboard';
   showMenu = false;
 
+  constructor() {
+  }
+  ngOnInit(): void {
+    const btn = <HTMLElement>document.querySelector('button.menu-button');
+    const menu = <HTMLElement>document.querySelector('.mobile-menu');
+    btn.addEventListener('click', () => {
+      menu.classList.toggle('hidden');
+    });  
+  }
   toggleNavbar() {
     this.showMenu = !this.showMenu;
   }
